7628e33612fbcb5d014465ab654c71267e0298d8
wyizhi30 <wyizhi30@gmail.com>
2023-05-31 09:48:01 +0800

    v.2

--- app/src/main/java/com/example/wyizhi30/MainActivity.kt
+++ app/src/main/java/com/example/wyizhi30/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.wyizhi30
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.wyizhi30.ui.theme.汪奕幟Theme
 
@@ -28,7 +34,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "作者：汪奕幟")
+    Image(
+        painter = painterResource(id = R.drawable.map),
+        contentDescription = "Map Image"
+    )
 }
 
 @Preview(showBackground = true)
